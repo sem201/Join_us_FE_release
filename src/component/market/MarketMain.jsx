177f6bc155855ff510836591e37ac3c43801de07
sem201 <sem201@naver.com>
2024-11-08 22:23:06 +0900

    design: marketMain 요소 디자인 일부 완료 수정중

--- src/component/market/MarketMain.jsx
+++ src/component/market/MarketMain.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import * as S from "./styled";
+import stiker from "../../assets/img/Market_Item_stiker.png";
+import Theme from "../../assets/img/Market_Item_theme_cherryblossom.png";
+import Frame1 from "../../assets/img/Market_Item_Frame_1.png";
+import Frame2 from "../../assets/img/Market_Item_Frame_2.png";
 
 const MarketMain = () => {
   return (
     <S.MainContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
-      <S.CardContainer></S.CardContainer>
+      <S.CardContainer>
+        <S.ImgContainer>
+          <S.MarketItemImg src={stiker} />
+        </S.ImgContainer>
+        <S.ItemNameContainer>
+          <S.ItemName>조인 어스 스티커</S.ItemName>
+        </S.ItemNameContainer>
+        <S.ItemNameContainer>
+          <S.Point>1000p</S.Point>
+        </S.ItemNameContainer>
+      </S.CardContainer>
+      <S.CardContainer>
+        <S.ImgContainer>
+          <S.MarketItemImg src={Theme} />
+        </S.ImgContainer>
+      </S.CardContainer>
+      <S.CardContainer>
+        <S.ImgContainer>
+          <S.MarketItemImg src={Frame1} />
+        </S.ImgContainer>
+      </S.CardContainer>
+      <S.CardContainer>
+        <S.ImgContainer>
+          <S.MarketItemImg src={Frame2} />
+        </S.ImgContainer>
+      </S.CardContainer>
     </S.MainContainer>
   );
 };
--- src/component/market/styled.js
+++ src/component/market/styled.js
@@ -53,7 +53,7 @@ export const MainContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
-  gap: 10px;
+  gap: 25px;
   background-color: red;
   width: 100%;
   height: 300px;
@@ -63,6 +63,59 @@ export const MainContainer = styled.div`
 
 export const CardContainer = styled.div`
   background-color: blue;
-  width: 144px;
-  height: 150px;
+  width: calc(50% - 12.5px);
+  height: 161px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const ImgContainer = styled.div`
+  border-radius: 8px;
+  background: #fff;
+  box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  height: 108px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MarketItemImg = styled.img`
+  /* width: 100%; */
+  height: 90%;
+`;
+
+export const ItemNameContainer = styled.div`
+  display: inline-flex;
+  height: 24px;
+  padding: 2px 8px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 34px;
+  border: 1px solid #e0e0e0;
+  background: #fff;
+`;
+
+export const ItemName = styled.p`
+  margin: 0;
+  color: #2e302d;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+export const Point = styled.p`
+  color: #417e59;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  color: var(--Primary-color, #417e59);
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
 `;
